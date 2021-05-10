e833a36a40239a8eaa70a94a6df91661873f2169
GCapobianco29 <gabriel.capobianco29@gmail.com>
2021-05-10 13:29:20 -0600

    2

--- WorkShop7_v2/src/main/webapp/functions.js
+++ WorkShop7_v2/src/main/webapp/functions.js
@@ -34,16 +34,16 @@ function populateCustTable(json)
             temp += "<tr>";
             temp += "<td>"+c.CustomerId+"</td>";
             temp += "<td>"+c.CustFirstName+"</td>";
-            temp += "<td>"+c.CustLastName+"</td></tr>";
-            /*temp += "<td>"+c.CustAddress+"</td>";
+            temp += "<td>"+c.CustLastName+"</td>";
+            temp += "<td>"+c.CustAddress+"</td>";
             temp += "<td>"+c.CustCity+"</td>";
             temp += "<td>"+c.CustProv+"</td>";
             temp += "<td>"+c.CustPostal+"</td>";
-            temp += "<td>"+c.CustCountry+"</td>";
-            temp += "<td>"+c.CustHomePhone+"</td>";
+            temp += "<td>"+c.CustCountry+"</td></tr>";
+          /*  temp += "<td>"+c.CustHomePhone+"</td>";
             temp += "<td>"+c.CustBusPhone+"</td>";
-            temp += "<td>"+c.CustEmail+"</td>";*/
-            /*temp += "<td>"+c.agentId+"</td></tr>";*/
+            temp += "<td>"+c.CustEmail+"</td>";
+            temp += "<td>"+c.agentId+"</td></tr>";*/
         })
         document.getElementById("customers-table").innerHTML = temp;
     }
